f3aae25296ef35589b23d81eb9accf5d1a155fd2
MiqueiasLima <miqueiaslima@outlook.com>
2024-03-26 21:20:52 -0300

    Corrigindo Exceção Handler

--- src/main/java/me/dio/sdw2024/adapters/in/GlobalExceptionHandler.java
+++ src/main/java/me/dio/sdw2024/adapters/in/GlobalExceptionHandler.java
@@ -1,17 +1,16 @@
 package me.dio.sdw2024.adapters.in;
 
 import me.dio.sdw2024.domain.exception.ChampionNotFoundException;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    LoggerFactory.
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(ChampionNotFoundException.class)
     public ResponseEntity<ApiError> handleDomainException(ChampionNotFoundException domainError){
         return ResponseEntity.unprocessableEntity().body(
@@ -19,8 +18,8 @@ public class GlobalExceptionHandler {
         );
     }
 
-    @ExceptionHandler(ChampionNotFoundException.class)
-    public ResponseEntity<ApiError> handleDomainException(Exception domainError){
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleDomainException(Exception unexpectedError){
         return ResponseEntity.internalServerError().body(
                 new ApiError("Ops! Ocorreu um erro Inesperado")
         );
